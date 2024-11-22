ffaa74ef3e4975dbd50a88e7a0d415862fff9827
rajkive <mehetreraj2004@gmail.com>
2024-11-22 01:30:01 -0500

    color scheme, images and basic layout for into-div

--- backend/script.js
+++ backend/script.js
@@ -3,16 +3,28 @@ const mainData = `
       <img src="#" alt="logo">
       <nav>
         <ul class="nav-options">
-            <li><a>Experience</a></li>
-            <li><a>Skills</a></li>
-            <li><a>Projects</a></li>
-            <li><a>Contact Me</a></li>
+            <li><a id="exp"">Experience</a></li>
+            <li><a id="skill">Skills</a></li>
+            <li><a id="project">Projects</a></li>
+            <li><a id="contact">Contact Me</a></li>
       </ul>
       </nav>
     </header> 
 
     <main class="introduction">
-        <h1>Hello welcome to my personal website</h1>
+    <div class="intro-container">
+    <div class="text-content">
+        <h1 class="heading">Hello — </h1>
+        <p class="intro-text">I am Rajshree Mehetre a Computer Science major with a Business minor at the University of Massachusetts Amherst. I am passionate about <span class="highlight">cybersecurity</span>, <span class="highlight">solving problems</span>, cameras and <span class="highlight">everything tech</span>.
+    </p>
+    <div class="social-links">
+                <img src="/images/linkedin.png" alt="LinkedIn" class="social-icon">
+                <img src="/images/git.png" alt="GitHub" class="social-icon">
+                <img src="/images/insta.png" alt="Instagram" class="social-icon">
+            </div>
+    </div>
+    </div>
+    <img src="/images/my-image.jpg" alt="my image" class="my-image">
     </main>
 
     <div class="section" id="section1">
